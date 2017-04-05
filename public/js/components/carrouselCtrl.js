1c1e642d7901b607763ca97657fdfe19cfb735a8
bijay timilsina <bjtimi.007@gmail.com>
2017-04-05 22:32:28 +0200

    carrousel working. css rewriting

--- public/js/components/carrouselCtrl.js
+++ public/js/components/carrouselCtrl.js
@@ -1,11 +1,12 @@
 (function () {
-  angular
-  .module('Carrousel')
+  angular.module('Carrousel')
   .controller('SliderController', SliderController)
 
   function SliderController (ImageFactory) {
     let vm = this
+    vm.indexBlock = 0  // initial block of data to be shown
 
+    // calling factory to get json and bind it to a scope variable
     ImageFactory.getImages()
     .then((resp) => {
       vm.blocks = resp.data.Images
@@ -13,15 +14,23 @@
     })
     .catch(err => { console.log(err) })
 
+    // function to randomize image of the current block
     vm.randImgIndex = function (block) {
+      console.dir(block)
       return block.images[Math.floor(Math.random() * block.images.length)]
     }
-    function iterateImg (currentPos) {
-      if (currentPos > vm.blocks.length - 1) currentPos = 0
-      if (currentPos < 0) currentPos = vm.blocks.length - 1
+
+    // function that changes index value of current block on click
+    function iterateImg (nowIndex) {
+      vm.indexBlock += nowIndex
+      if (vm.indexBlock > vm.blocks.length - 1) vm.indexBlock = 0
+      if (vm.indexBlock < 0) vm.indexBlock = vm.blocks.length - 1
     }
 
-    vm.previous = function (pos) { }
-    vm.next = function (pos) { }
+    // button controls for next and previous blocks
+    vm.controls =  (event, changeBy) => {
+      event.preventDefault()
+      iterateImg(changeBy)
+    }
   }
 })()
