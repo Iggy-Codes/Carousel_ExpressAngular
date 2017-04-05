2e01405a80f820d39cee00b757e168af5d071b49
bijay timilsina <bjtimi.007@gmail.com>
2017-04-05 16:06:27 +0200

    Done: randomizing imgs ToDo: resolve >10  loops

--- public/js/components/carrouselCtrl.js
+++ public/js/components/carrouselCtrl.js
@@ -5,14 +5,19 @@
 
   function SliderController (ImageFactory) {
     let vm = this
-    vm.randNum = (imgArr) => Math.floor(Math.random() * imgArr.length)
 
     ImageFactory.getImages()
     .then((resp) => {
       vm.blocks = resp.data.Images
-      console.log(vm.blocks)
       return vm.blocks
     })
     .catch(err => { console.log(err) })
+
+    vm.randImgIndex = function (block) {
+      return block.images[Math.floor(Math.random() * block.images.length)]
+    }
+
+    vm.previous = function () {}
+    vm.next = function () {}
   }
 })()
