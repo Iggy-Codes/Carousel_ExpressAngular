70f4c258c5304c8519548d9a18ff7629fc5d4fc8
Bijay Timilsina <bjtimi.007@gmail.com>
2017-04-11 11:43:34 +0200

    randomizer function put inside the promise handler

--- public/js/components/carrouselCtrl.js
+++ public/js/components/carrouselCtrl.js
@@ -12,18 +12,17 @@
     ImageFactory.getImages()
     .then((resp) => {
       vm.blocks = resp.data.Images
+      // function to randomize image of the current block
+      vm.randImgIndex = (block) => block.images[Math.floor(Math.random() * block.images.length)]
       return vm.blocks
     })
     .catch(err => { console.log(err) })
 
-    // function to randomize image of the current block
-    vm.randImgIndex = (block) => block.images[Math.floor(Math.random() * block.images.length)]
-
     // function that changes index value of current block on click & disable condition
     function iterateImg (nowIndex) {
       vm.indexBlock += nowIndex
-      vm.indexBlock > (vm.blocks.length - 1) ? vm.disableNext = true : vm.disableNext = false
-      vm.indexBlock < 0 ? vm.disablePrev = true : vm.disablePrev = false
+      vm.indexBlock >= (vm.blocks.length - 1) ? vm.disableNext = true : vm.disableNext = false
+      vm.indexBlock <= 0 ? vm.disablePrev = true : vm.disablePrev = false
     }
 
     // button controls for next and previous blocks
